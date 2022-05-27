3b7d957be4604dd630388db4aa07600a79180b70
walker <100868154+wa1ker38552@users.noreply.github.com>
2022-05-27 09:15:31 -0700

    better framework for supporting more threads

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-import requests, time, random
+from threading import Thread
 from alive import keepAlive
+import requests
+import random
+import time
+import os
 
-interval = 0.5 # time between each word
-waittime = 2 # time it lingers on last word
 
-status = [
-  'really cool message'.split(),
-  'another really cool message'.split(),
-  'dumb message'.split(),
-  'troll yes'.split(),
-  'last message'.split()
-]
+def create_thread(content, emoji, token, interval=5):
+  global requests_sent
+  headers = {
+  'authorization': token,
+  'content-type': 'application/json',
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+  }
+  while True:
+    message = random.choice(content)
+    data = {'custom_status': {'text': message, 'emoji_name': emoji}}
+    request = requests.patch('https://discord.com/api/v9/users/@me/settings', headers=headers, json=data)
+    requests_sent += 1
+    if request.status_code != 200: 
+      time.sleep(5)
+    else: time.sleep(interval)
 
-headers = {
-  'authorization': 'YOUR DISCORD AUTH TOKEN',
-  'content-type': 'application/json'
-}
+if __name__ == '__main__':
+  requests_sent = 0
+  keepAlive()
+  
+  message_object = open('status_objects.txt', 'r').read().split('\n\n')
+  for message_obj in message_object:
+    o = message_obj.split('\n')
+    Thread(target=lambda: create_thread(o[:-2], o[len(o)-2], o[len(o)-1])).start()
 
-keepAlive()
-while True:
-  message = random.choice(status)
-  for char in message:
-    json = {
-      'custom_status': {
-        'text': char, 
-        'emoji_name': "👉" # whatever emoji you want
-      }
-    }
-    r = requests.patch('https://discord.com/api/v9/users/@me/settings', headers=headers, json=json)
-    time.sleep(interval)
-  time.sleep(waittime)
+  print('>> Finished setting threads!')
+  while True:
+    os.system('clear')
+    print(f'>> Requests sent: {requests_sent}')
+    time.sleep(4)
